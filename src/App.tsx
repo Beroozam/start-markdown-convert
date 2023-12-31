480301d383f5a210cd48c570b207fcacfdb3318f
behrouz <bmb.mm7@gmail.com>
2023-12-31 15:08:14 +0330

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,34 @@ import Markdown from 'markdown-to-jsx'
 import {useEffect, useState} from "react";
 import Code from "./code";
 
+const preprocessMarkdown = (content) => {
+  // Replace "1-" with "1." for ordered lists
+  const processedContent_1 = content.replace(/(\d+)-/g, '$1.');
+  const processedContent = processedContent_1.replace(/:/g, ':\n\n');
+  return processedContent;
+};
+
+const OlTag = ({children, ...props}) => {
+  return <ol {...props} style={{padding:"0"}}>
+    {children}
+  </ol>
+}
+
+const UlTag = ({children, ...props}) => {
+  return <ul {...props} style={{padding:"0"}}>
+    {children}
+  </ul>
+}
+
+const LiTag = ({children, ...props}) => {
+  return <li {...props} style={{paddingInlineStart:".375em"}}>
+    {children}
+  </li>
+}
+
 function App() {
   const [postContent,setPostContent] = useState<string>("")
+  const processedContent = preprocessMarkdown(postContent);
   useEffect(()=>{
     import('./article.md')
         .then(res=>{
@@ -17,17 +43,36 @@ function App() {
         })
   },[])
   return (
-    <>
-      <Markdown
-        options={{
-          overrides: {
-            Code:{
-              component: Code
-            }
-          },
-        }}
-      >{postContent}</Markdown>
-    </>
+    <div style={{display:"flex",justifyContent:"space-between",gap:"2em"}}>
+      <div style={{maxWidth:"45%",overflow:"hidden"}}>
+        <h1 style={{textDecoration:"underline"}}>The content of markdown(.md) file</h1>
+        <pre>
+          {postContent}
+        </pre>
+      </div>
+      <div style={{display:"block",width:"5px",height:"2000px",backgroundColor:"#000",maxWidth:"45%"}}></div>
+      <div>
+        <h1 style={{textDecoration:"underline"}}>The converted content to JSX</h1>
+        <Markdown
+            options={{
+              overrides: {
+                Code:{
+                  component: Code
+                },
+                ol:{
+                  component: OlTag
+                },
+                ul:{
+                  component: UlTag
+                },
+                li:{
+                  component: LiTag
+                }
+              },
+            }}
+        >{processedContent}</Markdown>
+      </div>
+    </div>
   )
 }
 
--- src/article.md
+++ src/article.md
@@ -11,7 +11,11 @@
 - Item 3
 - Item 4
 
-1. **Sort an Array**
+
+**React Component for Rendering Markdown**: **`markdown-to-jsx`** is a React component that converts Markdown content into React elements. It doesn't support embedding React components within the Markdown content itself.
+
+
+1- **Sort an Array**
 
 <Code language="javascript">
 //strings
@@ -27,7 +31,7 @@ return a - b;
 //[ 8, 87, 101 ]
 </Code>
 
-2. *Select a random element*
+2- *Select a random element*
 
 <Code language="javascript">
 const items = ["Ball", "Bat", "Cup"]
@@ -43,77 +47,4 @@ function reverseString(string) {
 }
 
 revereseString("Random String")
-</Code>
-
-4. Check if element has a class
-
-<Code language="javascript">
-const element = document.querySelector("#element")
-element.classList.contains("active")
-</Code>
-
-5. String interpolation
-
-<Code language="javascript">
-const name = "Jaya"
-console.log(`Hi, ${name}. You have ${2 ** 3} new notifications.`}
-//Hi, Jaya. You have 8 new notifications.
-</Code>
-
-6. Loop through an array
-
-<Code language="javascript">
-const cars = ["Ford", "BMW", "Audi" ]
-for (let car of cars) {
-  console.log(car)
-}
-
-/*
-Ford
-BMW
-Audi
-*/
-</Code>
-
-7. Get current time
-
-<Code language="javascript">
-const date = new Date()
-const currentTime = 
-  `${date.getHours()}:${date.getMintues()}:${date.getSeconds()}`
-
-console.log(currentTimes)
-//example output: "22:16:41"
-</Code>
-
-8. Replace part of a string
-
-<Code language="javascript">
-const string = "You are awesome."
-const replacedString = string.replace("You", "We")
-
-console.log(replacedString) //Output: "We are awesome"
-</Code>
-
-9. Destructing variable assignment
-
-<Code language="javascript">
-let profile = ['bob', 34, 'carpenter'];
-let [name, age, job] = profile;
-console.log(name);
-// bob
-</Code>
-
-10. Using the spread operator
-
-<Code language="javascript">
-let data = [1,2,3,4,5];
-console.log(...data);
-//  1 2 3 4 5
-let data2 = [6,7,8,9,10];
-let combined = [...data, ...data2];
-console.log(...combined);
-// 1 2 3 4 5 6 7 8 9 10
-console.log(Math.max(...combined));
-// 10
 </Code>
