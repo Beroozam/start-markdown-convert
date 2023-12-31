192da06f4866c9ccbcaf482cd82fbe5dda4db95c
behrouz <bmb.mm7@gmail.com>
2023-12-31 16:37:04 +0330

    add build dir to git ignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/start-markdown-convert",
   assetsInclude:[
       "**/*.md"
   ],
   build: {
-    outDir: 'build', // Adjust this path to match your desired build directory
+    outDir: 'build', // Adjust this path to match your desired build directory,
   },
 })
